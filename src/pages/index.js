d42b000ee3c23d0e373c3ab81760a4dfd2c44f0c
Sourabh Patil <88647737+sourabh-jtc@users.noreply.github.com>
2021-11-25 10:24:01 +0530

    static queries (#3)

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,7 @@ import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css" 
 
-export default function Home({data}) {
-  const { title, description } = data.site.siteMetadata
+export default function Home() {
   return (
     <Layout>
       <section className = {styles.header}>
@@ -15,20 +14,8 @@ export default function Home({data}) {
           <Link className={styles.btn} to='/projects'> My portfolio</Link>
         </div>
         <img src='/banner.png' alt='banner image' style = {{maxWidth:'100%'}} />
-        <p> {title} - {description} </p>
       </section>
     </Layout>
 
   )
 }
-
-export const query = graphql`
-  query SiteInfo {
-    site {
-      siteMetadata {
-        description
-        title
-      }
-    }
-  }
-`
