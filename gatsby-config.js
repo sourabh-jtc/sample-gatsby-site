2ed828ce425ab7a52228e8b947a39dd3654a8c26
sourabh-jtc <sourabh.patil@jalantechnologies.com>
2021-11-25 10:12:50 +0530

    site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata:{
+    title:"The Worth sayings",
+    description:"Content creation on Youtube Social media handle",
+    copyright:"This channel is copyright 2021 The Worth Sayings"
+  }
 }
