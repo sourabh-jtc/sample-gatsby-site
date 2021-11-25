29f744823f88f6d942f1f812a1ff9b97260df014
Sourabh Patil <88647737+sourabh-jtc@users.noreply.github.com>
2021-11-25 10:13:32 +0530

    site metadata (#2)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,34 @@
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css" 
 
-export default function Home() {
+export default function Home({data}) {
+  const { title, description } = data.site.siteMetadata
   return (
     <Layout>
-    <section className = {styles.header}>
-      <div>
-        <h2>Inspire</h2>
-        <h3>Believe & Achieve</h3>
-        <p>This is The Worth Sayings</p>
-        <Link className={styles.btn} to='/projects'> My portfolio</Link>
-      </div>
-      <img src='/banner.png' alt='banner image' style = {{maxWidth:'100%'}} />
-    </section>
+      <section className = {styles.header}>
+        <div>
+          <h2>Inspire</h2>
+          <h3>Believe & Achieve</h3>
+          <p>This is The Worth Sayings</p>
+          <Link className={styles.btn} to='/projects'> My portfolio</Link>
+        </div>
+        <img src='/banner.png' alt='banner image' style = {{maxWidth:'100%'}} />
+        <p> {title} - {description} </p>
+      </section>
     </Layout>
 
   )
 }
+
+export const query = graphql`
+  query SiteInfo {
+    site {
+      siteMetadata {
+        description
+        title
+      }
+    }
+  }
+`
